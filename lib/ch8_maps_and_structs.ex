1da86d45601f1b9544ea2ce709852d1c036aa1e2
Raymond Maung <raymaung@gmail.com>
2016-01-02 15:12:13 +1100

    Add query map and struct

---
+++ lib/ch8_maps_and_structs.ex
@@ -0,0 +1,3 @@
+defmodule Ch8.Subscriber do
+  defstruct name: "", paid: false, over_18: true
+end
---
+++ test/ch8_dict_maps_hash_test.exs
@@ -0,0 +1,70 @@
+defmodule Ch8DictMapsHashTest do
+  use ExUnit.Case
+
+  test "sum using dict" do
+    defmodule Sum do
+      def values(dict) do
+        dict |> Dict.values |> Enum.sum
+      end
+    end
+
+    hd = [ one: 1, two: 2, three: 3] |> Enum.into HashDict.new
+    assert Sum.values(hd) == 6
+
+    map = %{ four: 4, five: 5, six: 6 }
+    assert Sum.values(map) == 15
+  end
+
+  test "pattern matching and updating maps" do
+    person = %{ name: "Dave", height: 1.88 }
+    %{ name: a_name } = person
+    assert a_name == "Dave"
+
+    # %{ name: _, height: _ } = person
+    assert %{ name: "Dave" } = person
+
+  end
+
+  test "query map" do
+    people = [
+      %{ name: "Grumpy",    height: 1.24 },
+      %{ name: "Dave",      height: 1.88 },
+      %{ name: "Dopey",     height: 1.88 },
+      %{ name: "Shaquille", height: 2.16 },
+      %{ name: "Sneezy",    height: 1.28 }
+    ]
+
+    by_height = fn (%{height: height}) ->
+      height > 1.5
+    end
+
+    tall_people = people |> Enum.filter(by_height)
+    assert tall_people == [
+      %{ name: "Dave",      height: 1.88 },
+      %{ name: "Dopey",     height: 1.88 },
+      %{ name: "Shaquille", height: 2.16 }
+    ]
+  end
+
+  test "updating a map" do
+    m = %{ a: 1, b: 2, c: 3 }
+    m1 = %{ m | b: "two", c: "three" }
+    m2 = %{ m1 | a: "one" }
+
+    assert m1 == %{ a: 1, b: "two", c: "three" }
+    assert m2 == %{ a: "one", b: "two", c: "three"}
+  end
+
+  test "maps and structs" do
+    s1 = %Ch8.Subscriber{}
+    assert s1.name == ""
+
+    s2 = %Ch8.Subscriber { name: "Dave" }
+    assert s2.name == "Dave"
+    assert s2.paid == false
+    assert s2.over_18 == true
+  end
+
+
+end
+
