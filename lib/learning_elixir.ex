fdff77e6af6461915c51d227e834254ac7d5490d
Raymond Maung <raymaung@gmail.com>
2016-03-09 21:54:31 +1100

    Add cli

--- lib/learning_elixir.ex
+++ lib/learning_elixir.ex
@@ -1,2 +1,11 @@
 defmodule LearningElixir do
 end
+# defmodule Issues do
+#   use Application.Behaviour
+
+#   # See http://elixir-lang.org/docs/stable/Application.Behaviour.html
+#   # for more information on OTP Applications
+#   def start(_type, _args) do
+#     Issues.Supervisor.start_link
+#   end
+# end
